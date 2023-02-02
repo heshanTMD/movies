646387738a49cfc586de3cdc41fac183d82f9e37
M.D.Heshan Sankalpa <69895232+heshanTMD@users.noreply.github.com>
2023-02-02 13:01:17 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+})
+
+let searchForm = document.querySelector('.search-from');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
